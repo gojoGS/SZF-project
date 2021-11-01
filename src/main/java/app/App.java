e8df65a67d9c297c207324ba24dba5c51fd4ce5a
Bartha Zoltán <barthazoli00@gmail.com>
2021-11-02 00:20:25 +0100

    Added base project

---
+++ src/main/java/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("""
+                Béreslegény, jól megrakd a szekeret,
+                sarjútüske böködi a tenyered.
+                Mennél jobban böködi a tenyered,
+                annál jobban rakd meg a szekeredet.
+                """);
+    }
+}
